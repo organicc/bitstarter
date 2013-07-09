3302ad0154407333cdbefcb7759d2de19e21e7cb
Enrique Mandujano <enrique.mandujano@gmail.com>
2013-07-09 19:26:14 +0200

    web.js modified to read index.html

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World 2!');
+// });
+
+var index = fs.readFileSync('index.html');
+out = buf.toString('utf-8',0,buf.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
